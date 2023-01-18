23182774401d7bb372b41a3872c1e9d574ee40bc
Илья <gagarinila@yandex.ru>
2023-01-18 18:52:33 +0500

    Create tests

--- src/test/java/tests/WikipediaAndroidTests.java
+++ src/test/java/tests/WikipediaAndroidTests.java
@@ -30,41 +30,41 @@ public class WikipediaAndroidTests extends TestBase {
         });
     }
 
-    @Tag("Mobile")
-    @Owner("Илья Гагарин")
-    @Test
-    void addLanguageTest() {
-        back();
-        step("Поиск", () ->
-                $(AppiumBy.accessibilityId("Search Wikipedia")).click());
-        step("Добавление нового языка", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_lang_button")).click();
-            $$(AppiumBy.id("org.wikipedia.alpha:id/wiki_language_title")).findBy(text("ADD LANGUAGE")).click();
-            $$(AppiumBy.id("org.wikipedia.alpha:id/localized_language_name")).findBy(text("Deutsch")).click();
-        });
-        step("Проверка добавленного", () ->
-                $$(AppiumBy.className("android.widget.TextView")).findBy(text("Deutsch")).shouldBe(visible));
-    }
-
-    @Tag("Mobile")
-    @Owner("Илья Гагарин")
-    @Test
-    void countrySearchTest() {
-        back();
-        step("Ввод текста поиска", () -> {
-            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Japan");
-        });
-        step("Проверка кол-ва запросов", () -> {
-            $$(AppiumBy.className("android.widget.TextView"))
-                    .shouldHave(CollectionCondition.sizeGreaterThan(0));
-        });
-        step("Проверка найденого", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
-            $(AppiumBy.className("android.webkit.WebView")).click();
-            $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Japan"));
-        });
-    }
+//    @Tag("Mobile")
+//    @Owner("Илья Гагарин")
+//    @Test
+//    void addLanguageTest() {
+//        back();
+//        step("Поиск", () ->
+//                $(AppiumBy.accessibilityId("Search Wikipedia")).click());
+//        step("Добавление нового языка", () -> {
+//            $(AppiumBy.id("org.wikipedia.alpha:id/search_lang_button")).click();
+//            $$(AppiumBy.id("org.wikipedia.alpha:id/wiki_language_title")).findBy(text("ADD LANGUAGE")).click();
+//            $$(AppiumBy.id("org.wikipedia.alpha:id/localized_language_name")).findBy(text("Deutsch")).click();
+//        });
+//        step("Проверка добавленного", () ->
+//                $$(AppiumBy.className("android.widget.TextView")).findBy(text("Deutsch")).shouldBe(visible));
+//    }
+//
+//    @Tag("Mobile")
+//    @Owner("Илья Гагарин")
+//    @Test
+//    void countrySearchTest() {
+//        back();
+//        step("Ввод текста поиска", () -> {
+//            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+//            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Japan");
+//        });
+//        step("Проверка кол-ва запросов", () -> {
+//            $$(AppiumBy.className("android.widget.TextView"))
+//                    .shouldHave(CollectionCondition.sizeGreaterThan(0));
+//        });
+//        step("Проверка найденого", () -> {
+//            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
+//            $(AppiumBy.className("android.webkit.WebView")).click();
+//            $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Japan"));
+//        });
+//    }
 }
 
 
